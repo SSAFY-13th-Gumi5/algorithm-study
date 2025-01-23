a3d92921cc74366be1c1a1f2c2849c9a3f1978a7
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-01-23 22:53:00 +0900

    [Bronze V] Title: A+B, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		System.out.println(Integer.parseInt(str[0]) + Integer.parseInt(str[1]));
+	}
+
+}
