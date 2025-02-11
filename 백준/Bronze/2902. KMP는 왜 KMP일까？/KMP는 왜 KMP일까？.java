5bb6d8b998f88a1e1d64c4f146f7f06798931f6d
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-02-11 22:42:57 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 68 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split("-");
+		
+		for (int i = 0; i <input.length; i++) {
+			bw.append(input[i].charAt(0));
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
